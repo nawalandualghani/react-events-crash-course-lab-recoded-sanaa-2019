e8da4eb9626876d54f0249028b5dc1b76e3801b0
nawal abdualghani <nawalabdualghani@gmail.com>
2019-12-08 07:45:33 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -11,7 +11,7 @@ export default class ChromeBoisDomain extends Component {
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
   drawChromeBoiAtCoords(event.clientX, event.clientY)
-  
+
   }
 
   /* TODO: Create an event handler which, when fired, invokes the provided
